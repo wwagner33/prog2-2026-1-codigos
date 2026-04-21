c9e5c9bfbbebf20cea7cc1c51c6d304edef41d69
Wellington Wagner Ferreira Sarmento <wwagner@virtual.ufc.br>
2026-04-21 15:45:16 -0300

    Update aulas-algoritmos/pesquisa-e-big-o/n-esimo-fibonacci.js
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- aulas-algoritmos/pesquisa-e-big-o/n-esimo-fibonacci.js
+++ aulas-algoritmos/pesquisa-e-big-o/n-esimo-fibonacci.js
@@ -75,10 +75,13 @@ function fibonacciRuim(n) {
 */
 
 function fibonacciMemo(n, memo = {}) {
-    if(n<=1) return n;
-    if(memo[n]) return memo[n];
-    
-    memo[n] = fibonacciMemo(n-1, memo) + fibonacciMemo(n-2, memo);
+    if (n <= 1) return n;
+
+    if (Object.prototype.hasOwnProperty.call(memo, n)) {
+        return memo[n];
+    }
+
+    memo[n] = fibonacciMemo(n - 1, memo) + fibonacciMemo(n - 2, memo);
     return memo[n];
 }
 
